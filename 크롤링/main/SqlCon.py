67af4e66baccc67eef8317000959d24c5db8731c
song <chihyuk1223@gmail.com>
2022-01-07 15:17:25 +0900

    DB 테스트 완료

--- 크롤링/main/SqlCon.py
+++ 크롤링/main/SqlCon.py
@@ -2,7 +2,6 @@
 import mysql.connector
 class SqlCon:
     hostlink = 'finalproject-1.c4jotwudvoiz.ap-northeast-2.rds.amazonaws.com'
-    #conn = mysql.connector.connect(host = '127.0.0.1', port = '3306', user='root', password='1234', database='news_summary')  # 해당 인자도 다 변수로 사용할 수 있으면 좋다
     conn = mysql.connector.connect(host = hostlink, port = '3306', user='admin', password='12341234', database='News_Summary')  # 해당 인자도 다 변수로 사용할 수 있으면 좋다
 
     @staticmethod
